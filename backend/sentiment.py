a4eeebe7edc9272ecbcf59d085f2a7ba55202865
Luca Dürkop <lumarie.duerkop@gmail.com>
2026-03-07 15:53:15 +0100

    sentiment

--- backend/sentiment.py
+++ backend/sentiment.py
@@ -1,3 +1,4 @@
+from transformers import pipeline
 class SentimentAnalyzer:
     def __init__(self):
         # future: self.classifier = pipeline("sentiment-analysis", model="bhadresh-savani/distilbert-base-uncased-emotion")
@@ -12,7 +13,39 @@ class SentimentAnalyzer:
         # TODO: Implement local HuggingFace inference here.
         # It's kept separate to avoid blocking the main textual LLM and 
         # to allow specialized architectures (like running on a local GPU).
-        return {
-            "label": "neutral",
-            "score": 0.5
-        }
+
+
+        # 1) General emotion model
+        emotion_clf = pipeline(
+            "text-classification",
+            model="j-hartmann/emotion-english-distilroberta-base",
+            top_k=None
+        )
+
+        # 2) Fine-grained emotion model
+        goemotion_clf = pipeline(
+            "text-classification",
+            model="SamLowe/roberta-base-go_emotions",
+            top_k=None
+        )
+
+        emotion_out = emotion_clf(text)[0]
+        goemotion_out = goemotion_clf(text)[0]
+
+        threshold = 0.5
+
+        filtered_emotions = sorted(
+        [e for e in emotion_out if e["score"] >= threshold] +
+        [e for e in goemotion_out if e["score"] >= threshold],
+        key=lambda x: x["score"],
+        reverse=True
+        )
+
+        filtered_emotions = [e["label"] for e in filtered_emotions]
+        emotion_str = ", ".join(filtered_emotions)
+
+        print(emotion_str)
+        print(filtered_emotions)
+        print("Emotion:", emotion_out)
+        print("GoEmotions:", goemotion_out)
+        return filtered_emotions
