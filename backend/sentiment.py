b5b040b287d32589319277a1fc6b7ee49173326a
Luca Dürkop <lumarie.duerkop@gmail.com>
2026-03-09 17:16:49 +0100

    Menthal Ilness added

--- backend/sentiment.py
+++ backend/sentiment.py
@@ -18,17 +18,26 @@ class SentimentAnalyzer:
             model="SamLowe/roberta-base-go_emotions",
             top_k=None
         )
+        
+        #3) Menthal ilness detection model
+        MODEL_NAME = "dsuram/distilbert-mentalhealth-classifier"
+
+        classifier = pipeline("text-classification", model=MODEL_NAME, top_k=None)
+
 
         emotion_out = emotion_clf(text)[0]
         goemotion_out = goemotion_clf(text)[0]
+        mental_health_out = classifier(text)[0]
 
 
         top_emotion = max(emotion_out, key=lambda x: x["score"])
 
-        k = 3
+        k = 2
         goemotion_filtered = sorted(goemotion_out, key=lambda x: x["score"], reverse=True)[:k]
+        
+        top_mental_health = max(mental_health_out, key=lambda x: x["score"])
 
-        combined = [top_emotion] + goemotion_filtered
+        combined = [top_emotion] + goemotion_filtered + [top_mental_health]
 
 
         filtered_emotions = list(dict.fromkeys(e["label"] for e in combined))
@@ -36,4 +45,5 @@ class SentimentAnalyzer:
         print("Filtered emotions:", filtered_emotions)
         print("Emotion:", emotion_out)
         print("GoEmotions:", goemotion_out)
+        print("Mental Health:", mental_health_out)
         return filtered_emotions
